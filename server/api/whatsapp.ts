db75027719adf21ff2c0f4d021adbcf97ba2c797
balsipson <thirdi@gmail.com>
2022-11-07 13:36:05 +0530

    updated whatsapp api

--- server/api/whatsapp.ts
+++ server/api/whatsapp.ts
@@ -5,6 +5,31 @@ import { useBody } from 'h3'
 export default defineEventHandler((event) => {
     const query = getQuery(event)
     console.log(query)
+    // Parse params from the webhook verification request
+    let mode = query["hub.mode"];
+    let token = query["hub.verify_token"];
+    let challenge = query["hub.challenge"];
+    
+    console.log(mode, token, challenge);
+    // // Check if a token and mode were sent
+    if (mode && token) {
+        // Check the mode and token sent are correct
+        if (mode == "subscribe" && token == "foxy") {
+            // Respond with 200 OK and challenge token from the request
+            console.log("WEBHOOK_VERIFIED");
+            // return challenge;
+            return {
+                headers: { 'Content-Type': 'application/json' },
+                statusCode: 200,
+                body: query["hub.challenge"]
+            };
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            return {
+                statusCode: 403
+            };
+        }
+    }
     return {
         headers: { 'Content-Type': 'application/json' },
         statusCode: 200,
@@ -12,28 +37,3 @@ export default defineEventHandler((event) => {
     }
 })
 
-    // Parse params from the webhook verification request
-    // let mode = context.params.hub.mode;
-    // let token = context.params.hub.verify_token;
-    // let challenge = context.params.hub.challenge;
-
-    // console.log(mode, token, challenge);
-    // // Check if a token and mode were sent
-    // if (mode && token) {
-    //     // Check the mode and token sent are correct
-    //     if (mode == "subscribe" && token == verify_token) {
-    //         // Respond with 200 OK and challenge token from the request
-    //         console.log("WEBHOOK_VERIFIED");
-    //         // return challenge;
-    //         return {
-    //             headers: { 'Content-Type': 'application/json' },
-    //             statusCode: 200,
-    //             body: Buffer.from(challenge)
-    //         };
-    //     } else {
-    //         // Responds with '403 Forbidden' if verify tokens do not match
-    //         return {
-    //             statusCode: 403
-    //         };
-    //     }
-    // }
